0d324c58271450b7b45038f060535dce0ee86631
Ismino <denise_cigel@hotmail.com>
2023-08-21 14:44:18 +0000

    Add in admin and setup adminpanel

--- hg/admin.py
+++ hg/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+#Code taken from CodeInstitute walktrough on a Django blog
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_data')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_data')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_data', 'approved')
+    list_filter = ('approved', 'created_data')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- hg/models.py
+++ hg/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+#This part of the code is from the CodeInstitutes Django Blog walkthrough
+#The code is the same as in the walktrough with some modifications on the names 
+#When i reserched how to make models for a Django blog app they all resulted in the same way
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_data = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_data = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_data"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_data = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_data"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
